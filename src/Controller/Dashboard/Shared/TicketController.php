b33175aebda583f547260a8a52aee297957b91d9
Md. Rawshan Kabir <rawshankabir0@gmail.com>
2024-03-20 16:31:59 +0600

    [Ticket Send By Mail]

--- src/Controller/Dashboard/Shared/TicketController.php
+++ src/Controller/Dashboard/Shared/TicketController.php
@@ -43,29 +43,61 @@ class TicketController extends Controller
         // Get the 'id' parameter from the URL
         $ref_id = $request->query->get('id');
 
-        // Execute raw SQL query to fetch data from event_mail table
-        $sql = "SELECT * FROM event_mails WHERE event_ref_id = :ref_id AND send_chanel = 'email'";
+        $sql = "SELECT * FROM eventic_order_ticket WHERE reference = :ref_id";
         $params = ['ref_id' => $ref_id];
         $statement = $connection->prepare($sql);
         $statement->execute($params);
-        $eventMails = $statement->fetchAll();
+        $order = $statement->fetch();
 
-        foreach ($eventMails as $eventMail) {
-            $email = (new \Swift_Message($translator->trans("Ticket Mail server test email")))
-                ->setFrom($services->getSetting('no_reply_email'), $services->getSetting('website_name'))
-                ->setTo($eventMail['email'])
-                ->setBody($templating->render('Dashboard/Shared/SendTicket/mailEventTickets.html.twig'), 'text/html');
+        $orderelement_id = $order['orderelement_id'];
 
-            try {
-                $result = $mailer->send($email);
-                if ($result == 0) {
-                    $this->addFlash('danger', $translator->trans("The email could not be sent"));
-                } else {
-                    $this->addFlash('success', $translator->trans("The test email has been sent, please check the inbox of") . " " . $eventMail['email']);
-                }
-            } catch (\Exception $e) {
+        $sql1 = "SELECT * FROM eventic_order_element WHERE id = :orderelement_id";
+        $params1 = ['orderelement_id' => $orderelement_id];
+        $statement1 = $connection->prepare($sql1);
+        $statement1->execute($params1);
+        $order_element = $statement1->fetch();
+
+        $order_id = $order_element['order_id'];
+
+        $sql2 = "SELECT * FROM eventic_order WHERE id = :order_id";
+        $params2 = ['order_id' => $order_id];
+        $statement2 = $connection->prepare($sql2);
+        $statement2->execute($params2);
+        $order = $statement2->fetch();
+
+        $user_id = $order['user_id'];
+
+        $sql3 = "SELECT * FROM eventic_user WHERE id = :user_id";
+        $params3 = ['user_id' => $user_id];
+        $statement3 = $connection->prepare($sql3);
+        $statement3->execute($params3);
+        $user = $statement3->fetch();
+
+
+        $orders = $services->getOrders(array("reference" => $order['reference']))->getQuery()->getOneOrNullResult();
+        if (!$orders) {
+            $this->addFlash('error', $translator->trans('The order can not be found'));
+            return $this->redirectToRoute("dashboard_attendee_orders");
+        }
+        $eventDateTicketReference = $request->query->get('event', 'all');
+
+        // Send Email
+        $email = new \Swift_Message($translator->trans("Mail server test email"));
+        $email->setFrom($services->getSetting('no_reply_email'), $services->getSetting('website_name'))
+            ->setTo($user['email'])
+            ->setBody($templating->render('Dashboard/Shared/SendTicket/mailEventTickets.html.twig',[
+                'order' => $orders,
+                'eventDateTicketReference' => $eventDateTicketReference,
+            ]), 'text/html');
+        try {
+            $result = $mailer->send($email);
+            if ($result == 0) {
                 $this->addFlash('danger', $translator->trans("The email could not be sent"));
+            } else {
+                $this->addFlash('success', $translator->trans("The test email has been sent, please check the inbox of") . " " . $user['email']);
             }
+        } catch (\Exception $e) {
+            $this->addFlash('danger', $translator->trans("The email could not be sent"));
         }
         return $this->redirectToRoute('send_tickets');
     }
