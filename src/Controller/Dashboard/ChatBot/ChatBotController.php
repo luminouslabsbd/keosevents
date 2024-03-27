a4f9407154588db372d1fb314a44dcc82c1526fe
Shohag Rana <shohagranaoo6@gmail.com>
2024-03-27 15:44:40 +0600

    chatbot train

--- src/Controller/Dashboard/ChatBot/ChatBotController.php
+++ src/Controller/Dashboard/ChatBot/ChatBotController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\MultipartStream;
+use GuzzleHttp\Psr7\Stream;
 
 class ChatBotController extends Controller
 {
@@ -53,39 +56,101 @@ class ChatBotController extends Controller
         }
 
 
+
+
+
         if ($type === "attachment") {
+
             $data = $request->request->all();
 
+            // $client = new Client();
+            // if ($request->files->has('file')) {
+            //     $file = $request->files->get('file');
+            //     $fileContent = file_get_contents($file->getPathname());
+            // }
+            // try {
+            //     $multipart = [
+            //         [
+            //             'name'     => 'bot_select',
+            //             'contents' => json_encode($data['bot_select'])
+            //         ],
+            //         [
+            //             'name'     => 'text',
+            //             'contents' => json_encode($data['text'])
+            //         ],
+            //         [
+            //             'name'     => 'file',
+            //             'contents' => $fileContent,
+            //             'filename' => $file->getClientOriginalName()
+            //         ]
+            //     ];
+
+            //     $response = $client->request("POST", $_ENV['CHAT_BOT_TRAIN_ATTACH'], [
+            //         'multipart' => $multipart,
+            //     ]);
+            // } catch (\Throwable $th) {
+            //     throw $th;
+            // }
+            // dd($response->getBody()->getContents());
+
+
+
+
+            // $data = $request->request->all();
             // if ($request->files->has('file')) {
             //     $files = $request->files->get('file');
-
-            //     foreach ($files as $file) {
-            //         try {
-            //             $response = $this->client->request("POST", "https://voicebot.keoscx.com/keosgpt/api/chatbot-create-attch", [
-            //                 'body' => [
-            //                     'file' => fopen($file->getPathname(), 'r'),
-            //                     'name' => 'hello'
-            //                 ],
-            //             ]);
-
-            //             // Handle API response for each file
-            //             $statusCode = $response->getStatusCode();
-            //             $content = $response->getContent();
-            //             dd($content);
-
-            //             // Process response...
-            //         } catch (Exception $e) {
-            //             dd($e->getMessage());
-            //         }
-            //     }
-            // } else {
-            //     // Handle case where 'file' is not found in the request
             // }
+            // $client = new Client();
+
+            // try {
+            //     $multipart = [];
+            //     for ($i = 0; $i < count($data['bot_select']); $i++) {
+            //         $fileContent = file_get_contents($files[$i]->getPathname());
+            //         $multipart[] = [
+            //             'name'     => 'bot_select[]',
+            //             'contents' => $data['bot_select'][$i]
+            //         ];
+            //         $multipart[] = [
+            //             'name'     => 'text[]',
+            //             'contents' => $data['text'][$i]
+            //         ];
+            //         $multipart[] = [
+            //             'name'     => 'file[]',
+            //             'contents' => $fileContent,
+            //             'filename' => $files[$i]->getClientOriginalName()
+            //         ];
+            //     }
+
+            //     $response = $client->request("POST", $_ENV['CHAT_BOT_TRAIN_ATTACH'], [
+            //         'multipart' => $multipart,
+            //     ]);
+            // } catch (\Throwable $th) {
+            //     throw $th;
+            // }
+            // dd($response->getBody()->getContents());
+
+
 
             $response = $this->client->request("POST", $_ENV['CHAT_BOT_TRAIN_TEXT'], [
                 'body' => $data,
             ]);
+
+            
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
        try {
             $chatbotData = $response->toArray();
