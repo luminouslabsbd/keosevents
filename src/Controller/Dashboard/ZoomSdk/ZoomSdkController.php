11cd5d1d342f0e925f7988d5d702ae6e15418fc4
Md. Rawshan Kabir <rawshankabir0@gmail.com>
2024-03-31 14:26:30 +0600

    [Link Done]

--- src/Controller/Dashboard/ZoomSdk/ZoomSdkController.php
+++ src/Controller/Dashboard/ZoomSdk/ZoomSdkController.php
@@ -8,9 +8,12 @@ class ZoomSdkController extends Controller
 {
   public function zoomSdkPlayer()
   {
+    
       return $this->render('Dashboard/ZoomSdk/zoom-sdk.html.twig',[
         'nodeServer' => $_ENV['NODE_SERVER'],
-        'quizApiUrl' => $_ENV['QUIZ_API']
+        'quizApiUrl' => $_ENV['QUIZ_API'],
+        'zoomAuthEndPoint' => $_ENV['ZOOM_AUTH_END_POINT'],
+        'sdkKey' => $_ENV['SDK_KEY'],
       ]);
 
   }
